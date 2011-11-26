9d502faf67d3fc91ed5e6d85a5892d75dad26ac3
Adi Dahiya <adidas006@gmail.com>
2011-11-25 19:11:36 -0500

    Created rails app and home/index controller & view

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PennBook::Application.config.secret_token = '0640c05a0e86dd6679b713467f00550f7a1162c95a55942bd9c79ef1ae2fcfe4b26dd3261d31733418ee19731510dba2038f11c3182b48dfbf54bfb06d8e61cf'
