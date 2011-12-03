02d5bc472b91f5e1b24715f82d03a2d06f7f1e3b
Ceasar Bautista <cbautista2010@gmail.com>
2011-12-03 15:47:46 -0500

    Revert "Added some basic validations to user model."
    This reverts commit e03c472693a6be71370dcff869d4e12a1ff4000a.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,3 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  validates :content, :length => { :maximum => 140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,2 @@
 class User < ActiveRecord::Base
-  validates :first_name, :presence => true,
-    :length => { :maximum => 50 }
-  validates :last_name, :presence => true,
-    :length => { :maximum => 50 }
-  validates :email, :presence => true,
-    :length => { :maximum => 50 },
-    :uniqueness => { :case_sensitive => false }
-  validates :birthday, :presence => true,
-    :length => { :maximum => 50 }
 end
