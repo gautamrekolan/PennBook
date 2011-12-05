8cbc72f0a1309b165a5fb804a5916009fa3494f4
Ceasar Bautista <cbautista2010@gmail.com>
2011-12-05 07:12:14 -0500

    Fixed a bunch of stuff I accidentally broke with validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,13 +13,15 @@
 
 class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :email, :birthday
-  validates :name,  :presence => true,
-                    :length   => { :maximum 63 }
+  validates :first_name,  :presence => true,
+                          :length   => { :maximum => 63 }
+  validates :last_name,   :presence => true,
+                          :length   => { :maximum => 63 }
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :email, :presence => true,
-                    :length   => { :maximum 63 },
+                    :length   => { :maximum => 63 },
                     :format   => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false}
   
