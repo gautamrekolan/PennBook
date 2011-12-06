571e85de6680130f7dcc9fc59f8a5c0befcc4106
Adi Dahiya <adidas006@gmail.com>
2011-12-06 16:15:44 -0500

    Added new post form to profile page, almost have posting working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
+    @post = Post.new
     @posts = @user.posts.paginate(:page => params[:page])
     @title = @user.first_name + " " + @user.last_name
 
