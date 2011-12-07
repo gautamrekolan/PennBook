1a779225e49cee1c1d8cb4653ac5802bb79e3844
Adi Dahiya <adidas006@gmail.com>
2011-12-07 04:28:21 -0500

    Got posting working, made better news feed view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,40 +1,4 @@
 class PostsController < ApplicationController
-  # GET /posts
-  # GET /posts.json
-  def index
-    @user = User.find(params[:user_id])
-    @posts = Post.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render :json => @posts }
-    end
-  end
-
-  # GET /posts/1
-  # GET /posts/1.json
-  def show
-    @user = User.find(params[:user_id])
-    @post = Post.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render :json => @post }
-    end
-  end
-
-  # GET /posts/new
-  # GET /posts/new.json
-  def new
-    @user = User.find(params[:user_id])
-    @post = Post.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render :json => @post }
-    end
-  end
-
   # GET /posts/1/edit
   def edit
     @user = User.find(params[:user_id])
@@ -52,24 +16,7 @@ class PostsController < ApplicationController
         format.html { redirect_to @user, :notice => 'Post was successfully created.' }
         format.json { render :json => @post, :status => :created, :location => @post }
       else
-        format.html { render :action => "new" }
-        format.json { render :json => @post.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /posts/1
-  # PUT /posts/1.json
-  def update
-    @user = User.find(params[:user_id])
-    @post = Post.find(params[:id])
-
-    respond_to do |format|
-      if @post.update_attributes(params[:post])
-        format.html { redirect_to @post, :notice => 'Post was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render :action => "edit" }
+        format.html { redirect_to @user, :notice => 'Post was unsuccessful.' }
         format.json { render :json => @post.errors, :status => :unprocessable_entity }
       end
     end
