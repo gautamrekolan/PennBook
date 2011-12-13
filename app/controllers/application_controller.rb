fcad40008fb4852084e2eccf71a45f7c80b943b4
Ceasar Bautista <cbautista2010@gmail.com>
2011-12-13 03:49:02 -0500

    Got users online working (although it's hacky.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_filter :touch
   protect_from_forgery
   include SessionsHelper
+
+  private
+
+  def touch
+    if signed_in?
+      current_user.touch
+    end
+  end
 end
